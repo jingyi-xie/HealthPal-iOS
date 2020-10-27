14fff3b03e533923c24b1ee25947a8dca6077ace
jx95 <jx95@duke.edu>
2020-10-27 02:55:20 -0400

    click health app

---
+++ HealthPal/DataController.swift
@@ -0,0 +1,36 @@
+//
+//  DataController.swift
+//  HealthPal
+//
+//  Created by Jaryn on 2020/10/27.
+//
+
+import UIKit
+
+class DataController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func clickHealthApp(_ sender: Any) {
+        print("click health app")
+        UIApplication.shared.open(URL(string: "x-apple-health://")!)
+
+    }
+    
+}
