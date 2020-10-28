0041c9ccf8c4d518cf3abb546c0df5ecf755e38c
jx95 <jx95@duke.edu>
2020-10-28 11:23:41 -0400

    added nav bar controller

--- HealthPal/SummaryController.swift
+++ HealthPal/SummaryController.swift
@@ -15,6 +15,16 @@ class SummaryController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    }
+    
 
     /*
     // MARK: - Navigation
